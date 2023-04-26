e19c1412e792dccb63baa4db6c9fe080b3291ce8
Kaj Fehlhaber <75676366+kejne@users.noreply.github.com>
2023-04-26 23:29:31 +0200

    First PR using the ensemble tool (#1)
    A crude version for trying out how git-go works

--- ensemble_test.go
+++ ensemble_test.go
@@ -28,12 +28,22 @@ func TestStart(t *testing.T) {
 
 		foundBranch := false
 		_ = branches.ForEach(func(reference *plumbing.Reference) error {
-			if getReferenceName(branchName) == reference.Name() {
-				err = e.repo.Storer.RemoveReference(reference.Name())
+			if e.wantedEnsembleBranchRef() == reference.Name() {
+				//err = e.repo.Storer.RemoveReference(reference.Name())
 				foundBranch = true
 			}
 			return nil
 		})
 		assert.True(t, foundBranch)
+	})
+}
+
+func TestNext(t *testing.T) {
+	t.Run("all changes added", func(t *testing.T) {
+		e, err := NewEnsemble(".")
+		assert.NoError(t, err)
+
+		err = e.Next()
+		assert.NoError(t, err)
 	})
 }
